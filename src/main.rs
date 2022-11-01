73f4f8335bc361f90c77d9d4c17d6bca1b94cf84
Arne Vogel <vogel@cs.fau.de>
2022-11-01 11:38:57 +0100

    arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Provide two numbers to add!");
+        return;
+    }
+
+    let res = args[1].parse::<i32>().unwrap_or(0) + args[2].parse::<i32>().unwrap_or(0);
+    println!("{res}");
 }
